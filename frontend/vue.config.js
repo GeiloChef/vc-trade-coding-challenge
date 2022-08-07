9654f2a926fb06c59e0c52faa9d83f5ba85fbb3e
Felix <felix.dziatkowski@googlemail.com>
2022-08-07 16:42:07 +0200

    fix public path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/vc-trade-coding-challange/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vc-trade-coding-challenge/" : "/",
 
   transpileDependencies: [
     'quasar'
